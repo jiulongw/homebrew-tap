712d0533d96bbaa076dec94e10841ac93f51ace8
Jiulong Wang <jiulongw@gmail.com>
2026-02-12 21:34:01 -0800

    xeno: update to v0.1.3 (#6)
    This PR updates the `xeno` formula for `v0.1.3`.
    
    - URL:
    `https://github.com/jiulongw/xeno/releases/download/v0.1.3/xeno-v0.1.3.tar.gz`
    - SHA256:
    `c9650c72a14cb86ef2d7aecd7f68af7579c0997634675758334b4f00fc3249cc`

--- Formula/xeno.rb
+++ Formula/xeno.rb
@@ -1,8 +1,8 @@
 class Xeno < Formula
   desc "Claude code based AI agent"
   homepage "https://github.com/jiulongw/xeno"
-  url "https://github.com/jiulongw/xeno/releases/download/v0.1.2/xeno-v0.1.2.tar.gz"
-  sha256 "18c69db4611c24ead1f84fe632142efd8f3d56a6e532601186e80980380debd2"
+  url "https://github.com/jiulongw/xeno/releases/download/v0.1.3/xeno-v0.1.3.tar.gz"
+  sha256 "c9650c72a14cb86ef2d7aecd7f68af7579c0997634675758334b4f00fc3249cc"
   head "https://github.com/jiulongw/xeno.git", branch: "main"
 
   depends_on "oven-sh/bun/bun"
